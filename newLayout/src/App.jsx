98d157aaf2b22740df586830623b8f4daa115c5c
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2023-10-01 21:20:07 -0300

    up

--- newLayout/src/App.jsx
+++ newLayout/src/App.jsx
@@ -15,7 +15,8 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="d-flex justify-content-center my-5 logotipo">
+
+        <div className="d-flex justify-content-center logotipo">
           <img src="./images/logotipo.svg" alt="Logotipo"/>
         </div>
 
@@ -24,11 +25,11 @@ function App() {
             <img className="rounded-start-pill border border-secondary shadow" src="./images/newImagePerfil.png" alt="Avatar PHBS" />
           </div>
           <div className="subtitles">
-            <h1 className="text-secondary display-6">O sucesso é a soma de pequenos esforços repetidos dia após dia.</h1>
-            <p className="text-white"><i class="bi bi-code-slash text-secondary"></i> Programador Web Full Stack PHP | React</p>
-            <p className="text-white"><i class="bi bi-brush text-secondary"></i> Web Designer UI | UX</p>
-            <p className="text-white"><i class="bi bi-hdd-network text-secondary"></i> SysAdmin Windows | Linux</p>
-            <p className="text-white"><i class="bi bi-music-note-beamed text-secondary"></i> Pianista em formação CEMVA</p>
+            <h1 className="text-secondary display-3 mb-3">Seja bem vindo!</h1>
+            <p className="text-white"><i class="bi bi-code-slash text-secondary"></i>Programador Web Full Stack PHP | React</p>
+            <p className="text-white"><i class="bi bi-brush text-secondary"></i>Web Designer UI | UX</p>
+            <p className="text-white"><i class="bi bi-hdd-network text-secondary"></i>SysAdmin Windows | Linux</p>
+            <p className="text-white"><i class="bi bi-music-note-beamed text-secondary"></i>Pianista em formação CEMVA</p>
           </div>
         </div>
 
@@ -41,10 +42,60 @@ function App() {
           </div>
         </div>
 
-        <div>
-          sdsdsds
+        <div className="d-flex justify-content-center my-5 btnSocial">
+          <a className="mx-4 fs-1 text-white" href="https://www.instagram.com/paulohbsouza.dev" target="_blank">
+            <i class="bi bi-instagram"></i>
+          </a>
+          <a className="mx-4 fs-1 text-white" href="https://www.facebook.com/paulohbsouza.dev" target="_blank">
+            <i class="bi bi-facebook"></i>
+          </a>
+          <a className="mx-4 fs-1 text-white" href="https://twitter.com/paulohbsouzadev" target="_blank">
+            <i class="bi bi-twitter-x"></i>
+          </a>
+          <a className="mx-4 fs-1 text-white" href="https://www.youtube.com/@paulohbsouza" target="_blank">
+            <i class="bi bi-youtube"></i>
+          </a>        
         </div>
-        
+
+        <div className="d-flex flex-column text-center my-5 text-white">
+
+          <a className="linkBtn text-white mb-5" href="https://www.linkedin.com/in/paulohbsouza/" target="_blank">
+            <div className="linkBtn-item p-3">
+              <i class="bi bi-linkedin"></i>Trajetória Profissional
+              <div className="text-secondary"><small>@paulohbsouza</small></div>
+            </div>
+          </a>
+
+          <a className="linkBtn text-white mb-5" href="https://github.com/paulohbsouza" target="_blank">
+            <div className="linkBtn-item p-3">
+              <i class="bi bi-github"></i>Portfólio Código Fonte
+              <div className="text-secondary"><small>@paulohbsouza</small></div>
+            </div>
+          </a>
+
+          <a className="linkBtn text-white mb-5" href="https://www.figma.com/community/file/1138607317570997425" target="_blank">
+            <div className="linkBtn-item p-3">
+              <i class="bi bi-palette-fill"></i>Weekly Planner Template
+              <div className="text-secondary"><small>@paulohbsouza</small></div>
+            </div>
+          </a>
+
+          <a className="linkBtn text-white mb-5" href="./pdf/test-protegido.pdf" target="_blank">
+            <div className="linkBtn-item p-3">
+              <i class="bi bi-filetype-pdf"></i>Resumo Currícular
+              <div className="text-secondary"><small>Paulo H B Souza</small></div>
+            </div>
+          </a>
+
+          <a className="linkBtn text-white mb-5" href="mailto:paulohbsouza.dev@gmail.com" target="_blank">
+            <div className="linkBtn-item p-3">
+              <i class="bi bi-envelope-at"></i>Contato / Orçamentos
+              <div className="text-secondary"><small>paulohbsouza.dev@gmail.com</small></div>
+            </div>
+          </a>
+
+        </div>
+
       </div>
     </>
   )
