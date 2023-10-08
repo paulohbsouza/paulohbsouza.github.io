904ebbd77c5166defd204d8318aef9cf4a4e70eb
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2023-10-08 12:54:26 -0300

    update

--- newLayout/src/App.jsx
+++ newLayout/src/App.jsx
@@ -2,13 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import './App.css'
 import { useState } from 'react'
+import Vitrine from './components/Vitrine'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAws, faBootstrap, faCpanel, faCss3Alt, faDev, faDocker, faFigma, faGit, faGitAlt, faGithub, faGithubAlt, faHtml5, faJs, faLaravel, faPhp, faPython, faReact, faSass, faUbuntu, faWordpress } from '@fortawesome/free-brands-svg-icons'
-
-
-function App() {
-
+export default function App() {
   //
   const [resumeClass, setResumeClass] = useState(false)
 
@@ -27,7 +23,7 @@ function App() {
       <div className="container">
 
         {/** idiomas */}
-        <div className="d-flex justify-content-end text-secondary my-3 idiomas">
+        <div className="d-flex justify-content-end text-secondary my-2 idiomas">
           <span className="mx-2"><img src="./images/icons8-brasil.png" alt="Brasil" title="Mudar idioma (pt-br)" /></span>
           <span className="mx-2"><img src="./images/icons8-eua.png" alt="EUA" title="Change language (en)" /></span>
         </div>
@@ -101,116 +97,7 @@ function App() {
         <hr />
 
         {/** vitrine */}
-        <div className="row my-5 vitrine">
-          <div className="col-md-12">
-            <div className="card mb-3 shadow">
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <div className="card-img card-img-1"></div>
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title display-6 text-secondary">Rocket</h5>
-                    <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <p className="card-text">
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faLaravel} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faReact} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faPhp} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faJs} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faGithub} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faWordpress} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faAws} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faDocker} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faHtml5} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faCss3Alt} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faSass} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faBootstrap} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faUbuntu} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faGitAlt} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faFigma} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faCpanel} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faDev} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faPython} /></span>
-                    </p>
-                    <button type="button" class="btn btn-outline-secondary">Espiar</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="card mb-3 shadow">
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <div className="card-img card-img-2"></div>
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title display-6 text-secondary">Armchair</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p className="card-text">
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faLaravel} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faReact} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faPhp} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faJs} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faGithub} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faWordpress} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faAws} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faDocker} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faHtml5} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faCss3Alt} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faSass} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faBootstrap} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faUbuntu} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faGitAlt} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faFigma} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faCpanel} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faDev} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faPython} /></span>
-                    </p>
-                    <button type="button" class="btn btn-outline-secondary">Espiar</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="card mb-3 shadow">
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <div className="card-img card-img-3"></div>
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title display-6 text-secondary">Scooter</h5>
-                    <p className="card-text">It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
-                    <p className="card-text">
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faLaravel} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faReact} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faPhp} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faJs} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faGithub} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faWordpress} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faAws} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faDocker} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faHtml5} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faCss3Alt} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faSass} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faBootstrap} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faUbuntu} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faGitAlt} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faFigma} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faCpanel} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faDev} /></span>
-                      <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faPython} /></span>
-                    </p>
-                    <button type="button" class="btn btn-outline-secondary">Espiar</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Vitrine />
 
         <hr />
 
@@ -224,5 +111,3 @@ function App() {
     </>
   )
 }
-
-export default App
---
+++ newLayout/src/components/Vitrine.jsx
@@ -0,0 +1,170 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faAws, faBattleNet, faBootstrap, faCloudscale, faCpanel, faCss3Alt, faDebian, faDev, faDocker, faFigma, faGitAlt,
+    faGithub, faGoogle, faHtml5, faJs, faLaravel, faLinux, faNode, faPhp, faPython, faReact,
+    faSass, faUbuntu, faWindows, faWordpress
+} from '@fortawesome/free-brands-svg-icons'
+import {} from '@fortawesome/free-regular-svg-icons'
+import { faBrush, faCloud, faCode, faCodeBranch, faCompassDrafting, faDatabase, faDesktop, faMobileScreen, faNetworkWired, faPalette, faPenRuler, faServer, faShield, faShieldHalved } from '@fortawesome/free-solid-svg-icons'
+
+
+export default function Vitrine() {
+    return (
+        <>
+            <div className="row my-5 vitrine">
+                <div className="col-md-12">
+                    <div className="card mb-3 shadow">
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <div className="card-img card-img-1"></div>
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title display-6 text-secondary">Back-end</h5>
+                                    <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+                                        printer took a galley of type and scrambled it to make a type specimen book. It has survived not 
+                                        only five centuries, but also the leap into electronic typesetting, remaining essentially 
+                                        unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem 
+                                        Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including 
+                                        versions of Lorem Ipsum.</p>
+                                    <p className="card-text">
+                                        <strong>Tags:</strong> Hosting, POO, Storage, Logs, Authentication, Algoritmo, Escalabilidade, 
+                                        Server Architecture, MVC, MySQL, MariaDB, Microserviços, Cybersecurity, Banco de Dados, Scrum, DevOps.
+                                    </p>
+                                    <p className="card-text">
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faPhp} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faLaravel} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faJs} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faNode} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faGithub} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faCodeBranch} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faWordpress} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faGitAlt} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faDev} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faDocker} /></span>
+                                    </p>
+                                    <button type="button" class="btn btn-outline-secondary">Pleno</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-12">
+                    <div className="card mb-3 shadow">
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <div className="card-img card-img-2"></div>
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title display-6 text-secondary">Front-end</h5>
+                                    <p className="card-text">
+                                        Seu escopo é a ponte que une os mundos do design e da programação. O profissional desenvolvedor 
+                                        front-end é o responsável por colocar em prática, através de códigos, o design de um site ou 
+                                        interface. Enquanto o web design projeta o visual de um site, o desenvolvimento front-end 
+                                        implementa esse design através de códigos, como HTML, CSS e JavaScript. Ou seja, é o front-end 
+                                        que coloca “para funcionar” toda a parte visual de uma interface, como a página de um site ou 
+                                        de um aplicativo. O front-end também deve ser capaz de se “comunicar” com a linguagem utilizada 
+                                        no back-end, que é a parte estrutural de sites, sistemas, aplicações, serviçoes e plataformas. 
+                                        Dessa forma, o visual de uma interface conseguirá ter suas funções correspondidas, como quando 
+                                        clicamos em um botão de “enviar mensagem” e a mensagem realmente é enviada.
+                                    </p>
+                                    <p className="card-text">
+                                        <strong>Tags:</strong> Design System, jQuery, Tailwind, Design Resposivo, Mobile First, SEO, 
+                                        Acessibilidade, Ajax.
+                                    </p>
+                                    <p className="card-text">
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faReact} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faJs} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faHtml5} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faCss3Alt} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faSass} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faBootstrap} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faDev} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faWordpress} /></span>
+                                    </p>
+                                    <button type="button" class="btn btn-outline-secondary">Pleno</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-12">
+                    <div className="card mb-3 shadow">
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <div className="card-img card-img-3"></div>
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title display-6 text-secondary">Design UI/UX</h5>
+                                    <p className="card-text">
+                                        O objetivo do UI design é criar uma interface visual atraente e fácil de usar, garantindo que 
+                                        os usuários possam interagir com o produto de forma eficiente e eficaz. Já o UX design 
+                                        abrange a experiência do usuário como um todo, desde o primeiro contato com o produto até o 
+                                        uso contínuo e a fidelização.
+                                    </p>
+                                    <p className="card-text">
+                                        <strong>Tags:</strong> Corel Draw, Screen Mockup, Canva, Figma, Logotipo, 
+                                        Adobe Illustrator, Análise Heurística.
+                                    </p>
+                                    <p className="card-text">
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faFigma} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faPalette} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faMobileScreen} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faBrush} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faDesktop} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faCompassDrafting} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faPenRuler} /></span>
+                                    </p>
+                                    <button type="button" class="btn btn-outline-secondary">Senior</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-12">
+                    <div className="card mb-3 shadow">
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <div className="card-img card-img-4"></div>
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title display-6 text-secondary">SysAdmin</h5>
+                                    <p className="card-text">
+                                        O escopo da administração de sistemas inclui instalar, suportar, e manter servidores e outros 
+                                        sistemas, planejar para responder a indisponibilidades de serviços e outros problemas. Outras 
+                                        tarefas incluem programação de scripts ou programas pequenos, gerenciamento de projetos para 
+                                        projetos relacionados a sistemas, supervisionar ou treinar técnicos de operação e de apoio, e 
+                                        consultoria para problemas de sistemas além do conhecimento do Técnico de Apoio ao Usuário ou 
+                                        Suporte de Primeiro Nível.
+                                    </p>
+                                    <p className="card-text">
+                                        <strong>Tags:</strong> Firewalls UTM, Proxy, Samba, DHCP, pfSense, VMware, Citrix 
+                                        Hypervisor/XenServer, VPN, Apache, IT Services, Backup, Routerboard, LAN, WAN, HTTPS.
+                                    </p>
+                                    <p className="card-text">
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faShieldHalved} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faCloud} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faUbuntu} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faWindows} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faDebian} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faCpanel} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faServer} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faLinux} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faDatabase} /></span>
+                                        <span className="text-Light fs-4 mx-1"><FontAwesomeIcon icon={faNetworkWired} /></span>
+                                    </p>
+                                    <button type="button" class="btn btn-outline-secondary">Senior</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
