6e0448d037ab0d0e000aa12fac155c04b2167dd2
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2023-10-25 10:44:14 -0300

    Update App.jsx

--- newLayout/src/App.jsx
+++ newLayout/src/App.jsx
@@ -54,7 +54,7 @@ export default function App() {
           <span className="text-white mb-3 fs-3" onClick={handleResume}><i class="bi bi-plus-circle"></i></span>
           <div className={`${resumeClass ? 'resume-in' : 'resume-out'} resume border border-secondary text-white fst-italic fw-light `}>
             Sou um profissional de TI com formação em Análise e Desenvolvimento de Sistemas. Tenho mais de 11 anos de experiência na área de infraestrutura de redes, tanto cabeadas quanto não cabeadas, além de expertise em administração e virtualização de servidores Windows e Linux, implantação de sistemas de gestão e gestão de serviços de TI.
-            Ao longo dos últimos dois anos, tenho me dedicado ao desenvolvimento web, atuando como programador full-stack e também com serviços em nuvem. Meu foco principal está nas tecnologias de desenvolvimento e design, incluindo Apache Server, SQL, ScriptCase, Laravel PHP, React JS, UI/UX Design, Bootstrap, SCSS, Git e GitHub.
+            Ao longo dos últimos três anos, tenho me dedicado ao desenvolvimento web, atuando como programador full-stack e também com serviços em nuvem. Meu foco principal está nas tecnologias de desenvolvimento e design, incluindo Apache Server, MySQL, Laravel PHP, React JS, UI/UX Design, Bootstrap, SCSS, Git e GitHub.
             Estou sempre em busca de atualizações e novos desafios para aprimorar minhas habilidades e oferecer soluções inovadoras.
           </div>
         </div>
