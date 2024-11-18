fa0298e9d42bd6bdfc73159e0ce537a5c18c49cc
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2024-11-17 21:46:50 -0300

    update

--- newLayout/src/components/Links.jsx
+++ newLayout/src/components/Links.jsx
@@ -3,6 +3,12 @@ export default function Links() {
     return (
         <>
             <div className="d-flex flex-column text-center my-5 text-white">
+                <a className="linkBtn text-white mb-5" href="https://reserva.ink/pontodev" target="_blank">
+                    <div className="linkBtn-item p-3">
+                        <i class="bi bi-bag-heart"></i>Loja online DevWear
+                        <div className="text-secondary"><small>reserva.ink/pontodev</small></div>
+                    </div>
+                </a>
                 <a className="linkBtn text-white mb-5" href="https://www.figma.com/community/file/1138607317570997425" target="_blank">
                     <div className="linkBtn-item p-3">
                         <i class="bi bi-calendar-week"></i>Weekly Planner Template
